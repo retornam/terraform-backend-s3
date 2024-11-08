ed189bc92a19132cb947d5240c16234df111fd1d
retornam <retornam@users.noreply.github.com>
2024-11-07 16:48:47 -0800

    update: terraform and aws provider
        update terraform version to >=1.9.*
        update aws_provider version to  5.0
    
    Signed-off-by: Raymond Etornam <retornam@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.9.8"
 
   required_providers {
-    aws = ">= 4.14.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
   }
 }
